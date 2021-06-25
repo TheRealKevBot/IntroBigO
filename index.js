7d27dadae81a983a3ee5339087c0fa9a22577c28
TheRealKevBot <Kglas001@gmail.com>
2021-06-25 16:34:45 -0600

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+// Intro To Big O Notation 
+
+// O(1) => Constant Time 
+
+// O(log n) => Logarithmic Time 
+
+// O(n) => Linear Time 
+
+// O(n^2) or O(n²) → Quadratic Time 
+
+// O(n!) => Factorial time
